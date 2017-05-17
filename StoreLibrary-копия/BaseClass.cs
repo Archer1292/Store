905e91140f8cf4ad2c6b2ce47b734a3696dc5775
Archer1292 <archer1292@gmail.com>
2017-05-17 17:03:41 +0300

    без юнити

---
+++ StoreLibrary-копия/BaseClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreLibrary
+{
+    public class BaseClass
+    {
+        public Guid Id{get;private set;}
+        public BaseClass()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
